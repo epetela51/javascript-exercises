02c59b3aa7c7905e90821b9ba3d5d81fdbab7732
Eric Petela <epetela51@gmail.com>
2022-03-03 22:04:20 -0500

    TDD practice for the first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let word = ""
+
+    for (let i = 0; i < num; i++) {
+        word += str;
+    }
+
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        return word;
+    }
 
 };
+
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
