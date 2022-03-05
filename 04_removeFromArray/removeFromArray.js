29d9aee2e9c3d58e64a2c27274c4922fc01a35e1
Eric Petela <epetela51@gmail.com>
2022-03-05 12:48:01 -0500

    update JS file with solution (couldn't solve) and passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+//Had to get solution this was to difficult
+
+const removeFromArray = function(...args) {
+
+    let array = args[0]
+
+    let newArray = [];
+
+    array.forEach(item => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    })
+
+    return newArray;
 
 };
+
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
