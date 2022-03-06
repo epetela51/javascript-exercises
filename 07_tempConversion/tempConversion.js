c6f46e9983d2e1f97040eefcb2028787d48c767e
Eric Petela <epetela51@gmail.com>
2022-03-05 20:44:17 -0500

    updated JS file and passed tests (solved myself)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,20 @@
-const ftoc = function() {
+// solved on my own
 
+const ftoc = function(temp) {
+  let conversion = (temp -32) /1.8;
+  let convertedNumber = Math.round(conversion * 10) / 10;
+  return convertedNumber;
 };
 
-const ctof = function() {
+ftoc(32);
 
+const ctof = function(temp) {
+  let conversion = (temp * 1.8) + 32;
+  let convertedNumber = Math.round(conversion * 10) / 10;
+  return convertedNumber;
 };
+
+ctof (0);
 
 // Do not edit below this line
 module.exports = {
