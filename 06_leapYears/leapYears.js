9513ee26d0bb0e1efe27153636aad68a8861a9e9
Eric Petela <epetela51@gmail.com>
2022-03-05 20:06:36 -0500

    Updated JS file and passed all tests (solved myself)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+// solved on my own
+
+const leapYears = function(year) {
+
+
+    if(year % 100 == 0) {
+
+        if (year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
+
+leapYears(1996);
 
 // Do not edit below this line
 module.exports = leapYears;
